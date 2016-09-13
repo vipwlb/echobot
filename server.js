0d56a4383877b5e942a812d65bb037132eb48b2c
vipwlb <vipwlb@gmail.com>
2016-09-13 00:34:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var builder = require('botbuilder');
 
 // Get secrets from server environment
 var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID, 
-    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
+    appId: '05357105-7d7c-496a-a389-54d055679fbb',//process.env.BOTFRAMEWORK_APPID, 
+    appSecret: 'DXDAwtBmN9Ue4f9Ke9cdbEG' 
 };
 
 // Create bot
